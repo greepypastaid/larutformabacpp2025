82e15cf4d792445e59c6f18c52721bd539a86dc1
greepypastaid <gangsar.reka@gmail.com>
2025-10-22 13:42:34 +0700

    feat: add latihan soal di array

---
+++ Array of CPP/LatihanSoal.cpp
@@ -0,0 +1,12 @@
+// Gas buatin kode buat nambahin sebuah data di array!
+// Syarat wajib pakai fungsi untuk penjumlahan (bebas mau pakai fungsi biasa atau void)!
+
+#include <iostream>
+
+using namespace std;
+
+int main () {
+
+
+    return 0;
+}
